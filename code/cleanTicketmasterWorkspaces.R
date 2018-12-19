ff0bb80cfcbf238422258722bfe85c8c65ccd056
g1mxb12 <mburge@frbchi.org>
2018-12-19 11:17:08 -0600

    working on tm data

--- code/cleanTicketmasterWorkspaces.R
+++ code/cleanTicketmasterWorkspaces.R
@@ -48,29 +48,43 @@ timestamp<-gsub(":",".", timestamp)
 date<-substr(x = timestamp,1,10)
 
 #dataframes
-publicSales <- eventData$sales$public
-dates<-eventData$dates
-promoter <- eventData$promoter
+#sales
+publicSales<- eventData$sales$public
+presales<-eventData$sales$presales
+presales<-rbindlist(presales, fill = TRUE)
+
+#dates
+startDates<-eventData$dates$start
+staus<- eventData$dates$status
+dropDatesDataCols <- c("start","status")
+dates<- eventData$dates[ , !(names(eventData$dates) %in% dropDatesDataCols)]
+
+#promoter
+promoter<-eventData$promoter
+
+#priceranges
+priceRanges<-rbindlist(eventData$priceRanges, fill = TRUE)
+
+#seatmap
 seatmap <- eventData$seatmap
+
+#
 ticketLimit <- eventData$ticketLimit
+
+
+#accessibility
 accessibility <- eventData$accessibility
 embedded <- eventData$`_embedded`
 links <- eventData$`_links`
-priceRange<- rbindlist(eventData$priceRanges, fill = TRUE)
 startDates <- eventData$dates$start
 status<-dates$status
 
-dropDatesDataCols <- c("start", "status")
-eventData$dates<- eventData$dates[ , !(names(eventData$dates) %in% dropDatesDataCols)]
 
-
-dropSalesDataCols <- c("presales")
-eventData$sales<- eventData$sales[ , !(names(eventData$sales) %in% dropSalesDataCols)]
-
-dropEventDataCols <- c("start","public","_embedded","_links","priceRanges", "products","startDates","promoters", "status", "dates", "classifications", "images","sales", "priceRange", "dates", "promoter", "seatmap","ticketLimit", "accessibility", "`_embedded`", "`_links`")
+dropEventDataCols <- c("info", "priceRanges", "presales", "pleaseNote","images", "sales", "classifications", "products", "promoters", "_links", "_embedded", "publicSales", "presales", "startDates", "status", "dates", "promoter", "priceRanges", "seatmap", "ticketLimit", "accessibility")
 eventData<- eventData[ , !(names(eventData) %in% dropEventDataCols)]
+
 #Combine dataframes to export
-finalEventData<-as.data.table(cbind(date,timestamp, eventData, publicSales, startDates, status, dates, promoter, ticketLimit, accessibility, priceRange, embedded, links))
+finalEventData<-as.data.table(cbind(date,timestamp, eventData, publicSales, startDates, status, dates, promoter, seatmap, ticketLimit, accessibility))
 
 outputFileName <- paste("C:/Users/g1mxb12/Desktop/nflTicketPrices/data/ticketmaster/cleanData/eventData/individual/tm_", timestamp, ".csv",sep="") 
 write.csv(finalEventData, file = outputFileName) 
