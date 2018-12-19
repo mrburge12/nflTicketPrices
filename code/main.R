15183a242a00d3dc0d5832c1a5ae09bb029bda8c
g1mxb12 <mburge@frbchi.org>
2018-12-19 16:05:04 -0600

    ran main script

---
+++ code/main.R
@@ -0,0 +1,10 @@
+
+filelist<- c("cleanWorkspaces.R", "mergeEventData.R")
+
+for(file in filelist) {
+  setwd('C:/Users/g1mxb12/Desktop/nflTicketPrices/code')
+  source(file)
+}
+
+#source('seatgeek.R')
+#source('ticketmaster.R')
