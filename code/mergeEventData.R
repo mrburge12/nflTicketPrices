5dea79f375326611886cc33aa817cfc1b8e4eb98
g1mxb12 <mburge@frbchi.org>
2018-12-19 15:43:03 -0600

    developing script to merge data

--- code/mergeEventData.R
+++ code/mergeEventData.R
@@ -1,26 +1,73 @@
+############################################################################################
+## Preliminaries
+############################################################################################
+
 library(jsonlite)
 library(data.table)
 library(dplyr)
 
-setwd('C:/Users/g1mxb12/Desktop/nflTicketPrices/data/seatgeek/cleanData/eventData/individual')
-path = 'C:/Users/g1mxb12/Desktop/nflTicketPrices/data/seatgeek/cleanData/eventData/individual'
-#out.file <- ""
-file.names <- dir(path, pattern =".csv")
+
+##############
+
+## SET FLAGS!
+
+##############
+
+sgFlag <- 1
+tmFlag <- 1
 
 
-#Initialize data to merge with by pulling in the first available data frame
-finalData <- read.csv(file.names[1])
-finalData<-as.data.table(finalData)
 
+############################################################################################
+## Merge the Data
+############################################################################################
 
-for(i in 2:length(file.names)){
-  data <- read.csv(file.names[i])
-  data <- as.data.table(data)
-  finalData<- rbind(data, finalData, fill = TRUE) 
-  finalData<- finalData %>%
-    select(noquote(order(colnames(finalData))))
-  finalData<- finalData[!duplicated(finalData),]
+if(sgFlag == 1) {
+  path = 'C:/Users/g1mxb12/Desktop/nflTicketPrices/data/seatgeek/cleanData/eventData/individual'
+  setwd(path)
+  file.names <- dir(path, pattern =".csv")
+  
+  
+  #Initialize data to merge with by pulling in the first available data frame
+  finalData <- read.csv(file.names[1])
+  finalData<-as.data.table(finalData)
+  
+  
+  for(i in 2:length(file.names)){
+    data <- read.csv(file.names[i])
+    data <- as.data.table(data)
+    finalData<- rbind(data, finalData, fill = TRUE) 
+    finalData<- finalData %>%
+      select(noquote(order(colnames(finalData))))
+    finalData<- finalData[!duplicated(finalData),]
+  }
+  
+  outputFileName <- paste("C:/Users/g1mxb12/Desktop/nflTicketPrices/data/seatgeek/cleanData/eventData/finalData",Sys.Date(),".csv",sep="") 
+  write.csv(finalData, file = outputFileName) 
+    
 }
 
-outputFileName <- paste("C:/Users/g1mxb12/Desktop/nflTicketPrices/data/seatgeek/cleanData/eventData/finalData",Sys.Date(),".csv",sep="") 
-write.csv(finalData, file = outputFileName) 
+
+if(tmFlag == 1) {
+  path = 'C:/Users/g1mxb12/Desktop/nflTicketPrices/data/ticketmaster/cleanData/eventData/individual'
+  setwd(path)
+  file.names <- dir(path, pattern =".csv")
+  
+  #Initialize data to merge with by pulling in the first available data frame
+  finalData <- read.csv(file.names[1])
+  finalData<-as.data.table(finalData)
+  
+  
+  
+  for(i in 2:length(file.names)){
+    data <- read.csv(file.names[i])
+    data <- as.data.table(data)
+    finalData<- rbind(data, finalData, fill = TRUE) 
+    finalData<- finalData %>%
+      select(noquote(order(colnames(finalData))))
+    finalData<- finalData[!duplicated(finalData),]
+  }
+  
+  outputFileName <- paste("C:/Users/g1mxb12/Desktop/nflTicketPrices/data/ticketmaster/cleanData/eventData/finalData",Sys.Date(),".csv",sep="") 
+  write.csv(finalData, file = outputFileName) 
+}
